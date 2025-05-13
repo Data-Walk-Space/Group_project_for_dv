80dbd139f8631c2383ccf9ae88e8fa2d70e6fd51
prostoadriana <adrianik0909@gmail.com>
2025-05-13 09:10:54 +0200

    code for maps for issue #6

--- data/maps_about_most _population.py
+++ data/maps_about_most _population.py
@@ -3,18 +3,15 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 most_ages = pd.read_excel('najcastejsia_vekova_skupina_obce.xlsx')
-most_jobs = pd.read_excel('najcastejsia_skupina_zamestnavania_obce.xlsx')
 most_title = pd.read_excel('najcastejsie_vzdelanie_obce.xlsx')
 
 
 most_ages.rename(columns={'Kód obce': 'code'}, inplace=True)
-most_jobs.rename(columns={'Kód obce': 'code'}, inplace=True)
 most_title.rename(columns={'Kód obce': 'code'}, inplace=True)
 
 
 map_sl = gpd.read_file('slovakia.json')
 map_ages_merge = map_sl.merge(most_ages, on='code')
-map_jobs_merge = map_sl.merge(most_jobs, on='code')
 map_title_merge = map_sl.merge(most_title, on='code')
 
 
@@ -28,16 +25,6 @@ legend.set_title("Vekové skupiny")
 plt.savefig('map_ages_popul.png')
 
 
-fig1, ax2 = plt.subplots(figsize=(16, 8))
-map_jobs_merge.plot(column='Najčastejšia skupina zamestnania', legend=True, cmap='plasma', ax=ax2)
-ax2.axis('off')
-ax2.set_title('Najčastejšia skupina zamestnania')
-legend = ax2.get_legend()
-legend.set_bbox_to_anchor((1.08, 0.2))
-legend.set_title("Zamestnanie")
-plt.savefig('map_jobs_popul.png')
-
-
 fig1, ax3 = plt.subplots(figsize=(16, 8))
 map_title_merge.plot(column='Najčastejšie vzdelanie', legend=True, cmap='Spectral', ax=ax3)
 ax3.axis('off')
@@ -49,5 +36,3 @@ legend.set_title("Vzdelanie")
 
 plt.savefig('map_title_popul.png')
 plt.show()
-
-
